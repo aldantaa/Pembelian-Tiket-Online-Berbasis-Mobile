627e4fe75545b8201421dbf3d4bb909ab0a58a42
Tiara Maya Lestari <tmlmaya66@gmail.com>
2026-09-24 09:20:22 +0700

    mencoba be

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController; // Wajib ditambahkan di Laravel 11
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index']);
+Route::get('/adminDashboard', [PageController::class, 'adminDashboard']);
+Route::get('/booking', [PageController::class, 'booking']);
+Route::get('/eTicket', [PageController::class, 'eTicket']);
+Route::get('/landing', [PageController::class, 'landing']);
+Route::get('/login', [PageController::class, 'login']);
+Route::get('/orderHistory', [PageController::class, 'orderHistory']);
+Route::get('/payment', [PageController::class, 'payment']);
+Route::get('/profile', [PageController::class, 'profile']);
+Route::get('/register', [PageController::class, 'register']);
+Route::get('/search', [PageController::class, 'search']);
+Route::get('/seatSelection', [PageController::class, 'seatSelection']);
+Route::get('/home', [PageController::class, 'home']);
